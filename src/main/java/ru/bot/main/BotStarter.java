b571b7d36a76cf2bbd75222ab676ad9bbce84ed5
maxim <maxim@maxim-Katana-GF76-12UC>
2023-04-26 19:15:01 +0300

    Removed anonymous classes

--- src/main/java/ru/bot/main/BotStarter.java
+++ src/main/java/ru/bot/main/BotStarter.java
@@ -14,40 +14,28 @@ public class BotStarter {
 	private static final Logger LOGGER = LoggerFactory.getLogger(BotStarter.class);
 
 	public static void start(String processId, AbstractStartCallback startCallback) {
-		Runtime.getRuntime().addShutdownHook(new Thread() {
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> startCallback.stop(processId, BotLock::unlock)));
 
-			@Override
-			public void run() {
-				startCallback.stop(processId, BotLock::unlock);
-			}
-		});
-
-		Executors.newScheduledThreadPool(1).scheduleAtFixedRate(new Runnable() {
-
-			@Override
-			public void run() {
-				if (!startCallback.isStarted()) {
-					if (!BotLock.tryLock(processId)) {
-						return;
-					}
-
-					LOGGER.info("Lock aquired");
-
-					try {
-						startCallback.start();
-					} catch (Exception e) {
-						LOGGER.error("Failed to start bot :(", e);
-						startCallback.stop(processId, BotLock::unlock);
-					}
-				} else {
-					if (!BotLock.isLocked(processId)) {
-						LOGGER.info("Lock lost. Bot is stopping");
-						startCallback.stop(processId, BotLock::unlock);
-					}
+		Executors.newScheduledThreadPool(1).scheduleAtFixedRate(()-> {
+			if (!startCallback.isStarted()) {
+				if (!BotLock.tryLock(processId)) {
+					return;
 				}
 
-			}
+				LOGGER.info("Lock aquired");
 
+				try {
+					startCallback.start();
+				} catch (Exception e) {
+					LOGGER.error("Failed to start bot :(", e);
+					startCallback.stop(processId, BotLock::unlock);
+				}
+			} else {
+				if (!BotLock.isLocked(processId)) {
+					LOGGER.info("Lock lost. Bot is stopping");
+					startCallback.stop(processId, BotLock::unlock);
+				}
+			}
 		}, 0, 5 * 1000L, TimeUnit.MILLISECONDS);
 	}
 }
