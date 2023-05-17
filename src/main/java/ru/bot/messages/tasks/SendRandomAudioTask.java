0c73b26d3a8ed88b183d60d25446ee4fe879b01b
Максим <demidovmax4@gmail.com>
2023-05-17 11:56:53 +0300

    Send random audio

---
+++ src/main/java/ru/bot/messages/tasks/SendRandomAudioTask.java
@@ -0,0 +1,55 @@
+package ru.bot.messages.tasks;
+
+import java.util.ArrayList;
+import java.util.random.RandomGeneratorFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendAudio;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+
+import ru.bot.chats.AllowableChatsHelper;
+import ru.bot.db.RedisHelper;
+
+public class SendRandomAudioTask extends AbstractSendTask {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SendRandomAudioTask.class);
+
+	private final TelegramLongPollingBot bot;
+
+	public SendRandomAudioTask(TelegramLongPollingBot bot) {
+		super("randomAudioTimes");
+		this.bot = bot;
+	}
+
+	@Override
+	public void run() {
+		try {
+			if (!canStartNow()) {
+				return;
+			}
+
+			var audios = RedisHelper.INSTANCE.getCollection("audios");
+			if (audios == null || audios.isEmpty()) {
+				return;
+			}
+
+			var audiosList = new ArrayList<>(audios);
+			var random = RandomGeneratorFactory.getDefault().create();
+			var text = RedisHelper.INSTANCE.getString("randomAudioMessage");
+
+			for (var chatId : AllowableChatsHelper.getAllowableChats()) {
+				var randomId = audiosList.get(random.nextInt(0, audiosList.size()));
+				bot.execute(new SendMessage(chatId, text));
+				bot.execute(new SendAudio(chatId, new InputFile(randomId)));
+			}
+
+		} catch (Exception e) {
+			LOGGER.error("failed to process task: " + getTaskName(), e);
+		}
+
+	}
+
+}
